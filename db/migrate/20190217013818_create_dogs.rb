354f6a9635519cdfe4d0b2672b8d321d2267aa90
Jessica Gladstone <jcg321@comcast.net>
2019-02-17 01:43:28 +0000

    Automatically backed up by Learn

--- db/migrate/20190217013818_create_dogs.rb
+++ db/migrate/20190217013818_create_dogs.rb
@@ -1,17 +1,5 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
-    
-    create_table :dogs do |t|
-      t.string :name
-      t.string :breed
-    end    
+ 
+  def change
   end
-  
-  def down
-    drop_table :dogs
-  end
-
-
-#   def change
-#   end
  end
